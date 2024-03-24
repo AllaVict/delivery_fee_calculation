6453369c54019c293ac65868c89d9b92dae678f6
AllaVict <allavsct@ukr.net>
2024-03-24 11:39:02 +0200

    init commit

---
+++ src/test/java/com/deliveryfeecalculation/DeliveryFeeCalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliveryfeecalculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryFeeCalculationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
